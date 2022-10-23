51de74c106bd7f5e2a142c59305dd55d274ce7cc
MohammedxSaad <moh.abdelhady01@gmail.com>
2022-10-23 12:40:34 +0200

    tani a3da

---
+++ front_end/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+const Navbar = () => {
+
+    return (
+        <header>
+            <nav className="navbar navbar-light bg-light">
+                <a className="navbar-brand" href="#">Navbar</a>
+            </nav>
+        </header>
+        
+    )
+}
+
+export default Navbar
