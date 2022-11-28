8c529723b2d2bddce23ddfc560cc8b30ec41ce79
MohammedxSaad <moh.abdelhady01@gmail.com>
2022-11-28 18:10:31 +0200

    x

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -14,6 +14,7 @@ import GetCoursesByInstructor from './pages/GetCoursesByInstructo';
 import SearchResults from './pages/SearchResults';
 import Profile from './pages/Profile';
 import ChangePassword from './pages/ChangePassword';
+import Contract from './pages/Contract';
 
 
 function App() {
